d8153e469cedb0ac84b1c81cb9309cb5556e7f60
beau-augusto <beausalgado@gmail.com>
2022-04-07 01:35:13 -0300

    testing the git connection

--- my-first-react-project/src/Web.js
+++ my-first-react-project/src/Web.js
@@ -2,7 +2,7 @@ import React from 'react';
 function Web(){
   return (
     <div className="container">
-     <h1>Bienvenido a nuestro primer proyecto en React</h1>
+     <h1>Bienvenido a nuestro primer proyecto en React test</h1>
     </div>
   )
 
